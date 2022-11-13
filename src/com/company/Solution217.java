2eb72910d27c1999b78c12c1df8ffd220bfc61ec
Tatiana <tshipilova392@yandex.ru>
2022-11-14 00:00:50 +0300

    217. ContainsDuplicate

--- src/com/company/Solution217.java
+++ src/com/company/Solution217.java
@@ -1,11 +1,16 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Solution217 {
     public boolean containsDuplicate(int[] nums) {
-        return true;
-    }
-
-    public static void main(String[] args) {
-
+        Arrays.sort(nums);
+        int equalAmount=0;
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i]==nums[i+1]){
+                equalAmount++;
+            }
+        }
+        return !(equalAmount == 0);
     }
 }
