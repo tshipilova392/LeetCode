8ae52a1850049b521425e3560a108358395e29ec
Tatiana <tshipilova392@yandex.ru>
2022-11-13 23:08:34 +0300

    first commit

---
+++ src/com/company/Solution217.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Solution217 {
+    public boolean containsDuplicate(int[] nums) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
