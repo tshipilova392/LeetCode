12c24d9ae3ee36626041e6b3bce344f5d31c4f71
Tatiana <tshipilova392@yandex.ru>
2022-12-21 21:08:52 +0300

    128LongestConsecutiveSequence

--- src/com/company/Solution125ValidPalindrome.java
+++ src/com/company/Solution125ValidPalindrome.java
@@ -5,12 +5,33 @@ import java.util.Locale;
 public class Solution125ValidPalindrome {
     public static boolean isPalindrome(String s) {
         //O(n) time
-        //O(n) memory
-        s = s.replaceAll("[^a-zA-Z\\d]","").toLowerCase();
-        StringBuilder sb=new StringBuilder(s);
-        sb.reverse();
-        String t=sb.toString();
-        return s.equals(t);
+        //O(1) memory
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+
+            Character start = s.charAt(i);
+            Character end = s.charAt(j);
+
+            if (!Character.isLetterOrDigit(start)) {
+                i++;
+                continue;
+            }
+
+            if (!Character.isLetterOrDigit(end)) {
+                j--;
+                continue;
+            }
+
+            if (Character.toLowerCase(start) != Character.toLowerCase(end)) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
